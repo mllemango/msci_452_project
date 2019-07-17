3aca48e4427e24cb2a49b70f4490fdbd739be35d
mllemango <msmangochan@gmail.com>
2019-07-17 19:52:20 -0400

    binomial!

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 import numpy as np
-n = 1
-p = 0.5
-fair_flips = np.random.binomial(n, p, 10)
-print(fair_flips)
+import operator as op
+from functools import reduce
+
+fair_flips = np.random.binomial(1, 0.5, 2)
+
+
+def ncr(n, r):
+    r = min(r, n - r)
+    numer = reduce(op.mul, range(n, n-r, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer / denom
+
+
+survey_pop = 3
+for i in range(survey_pop+1):
+
+    # is it accurate
+    '''
+    X = probability of current branch happening
+    Y = probability of market share greater than y%
+
+    variables
+    probability of X = Binomial
+    probability of Y and X = algebra
+    probability of Y given X = known value
+    '''
+
+    P_X = ncr(survey_pop, i) * (0.5*0.5*0.5)
+    print(P_X)
